81918220707635fce2fdc40f2f6bf9f24520fbe5
Aleksei Briukhachev <abriukhachev@trask.cz>
2023-02-17 15:22:27 +0100

    Fix method

--- SpringLibraryBoot/SpringLibraryBoot/src/main/java/com/aleksei/springcourse/springlibraryboot/services/PeopleService.java
+++ SpringLibraryBoot/SpringLibraryBoot/src/main/java/com/aleksei/springcourse/springlibraryboot/services/PeopleService.java
@@ -57,25 +57,19 @@ public class PeopleService {
     public List<Book> getBooksByPersonId(int id) {
         Optional<Person> person = peopleRepository.findById(id);
 
-//        if (person.isPresent()) {
-//            Hibernate.initialize(person.get().getBooks());
-//            // Мы внизу итерируемся по книгам, поэтому они точно будут загружены, но на всякий случай
-//            // не мешает всегда вызывать Hibernate.initialize()
-//            // (на случай, например, если код в дальнейшем поменяется и итерация по книгам удалится)
-//
-//            // Проверка просроченности книг
-//            person.get().getBooks().forEach(book -> {
-//                long diffInMillies = Math.abs(book.getTakenAt().getTime() - new Date().getTime());
-//                // 864000000 милисекунд = 10 суток
-//                if (diffInMillies > 864000000)
-//                    book.setExpired(true); // книга просрочена
-//            });
-//
-//            return person.get().getBooks();
-//        }
-//        else {
-//            return Collections.emptyList();
-//        }
-        return person.get().getBooks();
+        if (person.isPresent()) {
+            Hibernate.initialize(person.get().getBooks());
+            person.get().getBooks().forEach(book -> {
+                long diffInMillies = Math.abs(book.getTakenAt().getTime() - new Date().getTime());
+                // 864000000 милисекунд = 10 суток
+                if (diffInMillies > 864000000)
+                    book.setExpired(true);
+            });
+
+            return person.get().getBooks();
+        }
+        else {
+            return Collections.emptyList();
+        }
     }
 }
